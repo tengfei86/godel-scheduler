8cd47e587f63bb01a33b8f9360c9bfbfa190ad41
tong.gao <tong.gao@halliburton.com>
2026-09-09 15:45:34 +0800

    update examples for test

--- test/e2e/benchmark/run-all.sh
+++ test/e2e/benchmark/run-all.sh
@@ -22,11 +22,11 @@
 #   ./run-all.sh --groups "a b" --scales "s3" --workloads "w3" --instances "1 2 3 5"  # 水平扩展测试
 #   ./run-all.sh --groups "a b c d e" --scales "s2 s3" --workloads "w2 w3" --setup-nodes  # 所有调度器 × s2/s3 × w2/w3，自动创建节点
 #   ./run-all.sh --groups "a b c d e" --scales "s3 s4" --workloads "w1 w2 w3 w4 w5 w6 w7" --instances "1 3"   --setup-nodes 
-#   ./run-all.sh --groups "a b" --scales "s3 s4" --workloads "w3 w4 w5 w6 w7" --instances "3"   --setup-nodes 
+#   ./run-all.sh --groups "a b" --scales "s3 s4" --workloads "w3 w4 w5 w6 w7"  --redeploy-each-run  --instances "3"   --setup-nodes 
 #   ./run-all.sh --groups "a b" --scales "s3 s4" --workloads "w4" --instances "3"   --setup-nodes 
-#   ./run-all.sh --groups "a b c d e" --scales "s1" --workloads "w1"  --instances "1"
-#   ./run-all.sh --groups "a b c d e" --scales "s2 s3" --workloads "w2 w3" --instances "1" --setup-nodes
-#   ./run-all.sh --groups "a" --scales "s3" --workloads "w4" --instances "3" --setup-nodes --redeploy-each-run  # a × s3 × w4，每次 run 前重装
+#   ./run-all.sh --groups "a b c d e" --scales "s1" --workloads "w1" --redeploy-each-run  --instances "1"
+#   ./run-all.sh --groups "a b c d e" --scales "s2 s3" --workloads "w2 w3" --redeploy-each-run --instances "1" --setup-nodes
+#   ./run-all.sh --groups "a" --scales "s3" --workloads "w4" --instances "3" --setup-nodes --redeploy-each-run
 
  
 set -eu
